ba79a3b9c03f6b661a68b6a6cf4f58dea8ed1b59
Riley Fewell <rileyfewell@Rileys-MacBook-Pro.local>
2015-02-17 13:49:30 -0800

    Initial Commit

---
+++ ShufflePuzzle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShufflePuzzle
+//
+//  Created by Riley Fewell on 2/17/15.
+//  Copyright (c) 2015 Riley Fewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShufflePuzzle/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShufflePuzzle
+//
+//  Created by Riley Fewell on 2/17/15.
+//  Copyright (c) 2015 Riley Fewell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
